b69bc2ef55a000c1f7fca41734acc0e0412c19e3
Júlia fernandes Pereira <C3007878@fgv.edu.br>
2025-08-12 15:08:28 -0300

    3?4: funçao que sorteia 7 numeros e calcula media

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 print("Hello World")
+
+import random
  
 def cumprimento (texto):
     return f"Ola, {texto}"
+
+def media_7_numeros():
+    nums = [random.randint(1,100) for_in range(7)]
+    media = sum(nums) / len (nums)
+    return nums, media
+
+
 if __name__ == "__main__":
     nome_completo = "Julia Fernandes Pereira"
     print (cumprimento(nome_completo))
-    
+    nums, media = media_7_numeros()
+    print ("Numeros sorteados:", nums)
+    print ("Media:", media )
