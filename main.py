a03fce8235c47bec2d318615ad118fb83937c08f
Júlia fernandes Pereira <C3007878@fgv.edu.br>
2025-08-12 14:59:51 -0300

    2/4: funçao cumprimento e chamada com nome

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 print("Hello World")
-
-
-def cumprimento ():
-    nome= input ("Digite seu nome:")    
-    print(f'OLa, {nome}! Bem-vindo ao projeto!') 
-    s
+ 
+def cumprimento (texto):
+    return f"Ola, {texto}"
+if __name__ == "__main__":
+    nome_completo = "Julia Fernandes Pereira"
+    print (cumprimento(nome_completo))
+    
